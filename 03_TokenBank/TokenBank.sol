ee89768660c531695b98a592690c9ef6bf0cd714
0x.Sen <116653851+0xSen10@users.noreply.github.com>
2025-09-28 20:50:47 +0800

    Add files via upload

---
+++ 03_TokenBank/TokenBank.sol
@@ -0,0 +1,95 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+
+contract BaseERC20 {
+    string public name; 
+    string public symbol; 
+    uint8 public decimals; 
+
+    uint256 public totalSupply; 
+
+    mapping (address => uint256) balances; 
+
+    mapping (address => mapping (address => uint256)) allowances; 
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    constructor() public {
+        // write your code here
+        // set name,symbol,decimals,totalSupply
+        name = "BaseERC20";
+        symbol = "BERC20";
+        decimals = 18;
+        totalSupply = 100000000 * 10**uint256(decimals);
+        balances[msg.sender] = totalSupply;  
+        emit Transfer(address(0), msg.sender, totalSupply);
+    }
+
+    function balanceOf(address _owner) public view returns (uint256 balance) {
+        // write your code here
+        return balances[_owner];
+    }
+
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        // write your code here
+        require(balances[msg.sender] >= _value, "ERC20: transfer amount exceeds balance");
+
+        balances[msg.sender] -= _value;
+        balances[_to] += _value;
+
+        emit Transfer(msg.sender, _to, _value);  
+        return true;   
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        // write your code here
+        require(balances[_from] >= _value, "ERC20: transfer amount exceeds balance");
+        require(allowances[_from][msg.sender] >= _value, "ERC20: transfer amount exceeds allowance");
+        
+        balances[_from] -= _value;
+        balances[_to] += _value;
+        allowances[_from][msg.sender] -= _value;
+
+        
+        emit Transfer(_from, _to, _value); 
+        return true; 
+    }
+
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+        // write your code here
+        allowances[msg.sender][_spender] = _value;
+
+        emit Approval(msg.sender, _spender, _value); 
+        return true; 
+    }
+
+    function allowance(address _owner, address _spender) public view returns (uint256 remaining) {   
+        // write your code here     
+         return allowances[_owner][_spender];
+    }
+}
+
+contract TokenBank {
+    BaseERC20 public token;
+    constructor(address tokenAddress) {
+        token = BaseERC20(tokenAddress);
+    }
+    mapping(address => uint256) public balances;
+
+
+    function deposit(uint256 amount) external {
+        require(amount > 0, "Deposit amount must be greater than 0");
+        balances[msg.sender] += amount;
+        token.transferFrom(msg.sender, address(this), amount);
+
+    }
+
+    function withdraw(uint256 amount) external {
+        require(amount > 0, "Withdraw amount must be greater than 0");
+        require(balances[msg.sender] >= amount, "Insufficient balance");
+        balances[msg.sender] -= amount;
+        token.transfer(msg.sender, amount);
+    }
+}
